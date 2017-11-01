e2644be8f60591792a0e83a813c7eddb780defbf
Eric McKinney <mckinney99@gmail.com>
2017-11-01 20:16:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
-  def question?
 
+  def question?
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+  self.split(/[.?!]+/).count
   end
 end
